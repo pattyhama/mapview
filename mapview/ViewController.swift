60bf304d9811255e3907be6dee43715f1b702ec6
pattyhama <strodr@gmail.com>
2016-08-22 00:05:04 +0900

    add map view showing specified location

--- mapview/ViewController.swift
+++ mapview/ViewController.swift
@@ -13,16 +13,20 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let coordinate = CLLocationCoordinate2DMake(35.700668, 139.771670)
+        let span = MKCoordinateSpanMake(0.01, 0.01)
+        let region = MKCoordinateRegionMake(coordinate, span)
+        mapView.setRegion(region, animated:true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
