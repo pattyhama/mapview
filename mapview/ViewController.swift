e2a45564196fa10e627fbc702950cecbcd5c7806
pattyhama <strodr@gmail.com>
2016-08-21 19:45:51 +0900

    first commit

---
+++ mapview/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  mapview
+//
+//  Created by 浜岡春見 on 2016/08/21.
+//  Copyright © 2016年 pattyhama. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
